2118b5eae616c4c0a847fbe7a815a42aa1da6d7b
kovacsv <kowik1984@gmail.com>
2014-10-18 18:11:15 +0200

    Modify welcome message.

--- website/include/importerapp.js
+++ website/include/importerapp.js
@@ -65,8 +65,8 @@ ImporterApp.prototype.WelcomeDialog = function ()
 	var dialogText = [
 		'<div class="importerdialog">',
 		'<div class="welcometitle">Welcome to Online 3D Viewer!</div>',
-		'<div class="welcometext">Here you can view 3D models online. Just simply drag and drop 3D files to this browser window or use the open button above. The supported file formats are:</div>',
-		'<div class="welcometextformats">3ds, obj, stl</div>',
+		'<div class="welcometext">Here you can view 3D models online. Just simply drag and drop 3D files and textures to this browser window or use the open button above.</div>',
+		'<div class="welcometextformats">Supported formats: 3ds, obj, stl.</div>',
 		'<div class="welcometext">Powered by <a target="_blank" href="https://github.com/mrdoob/three.js/">Three.js</a> and <a target="_blank" href="https://github.com/kovacsv/JSModeler">JSModeler</a>.</div>',
 		'<div class="welcometext"><a target="_blank" href="https://github.com/kovacsv/Online3DViewer"><img src="images/githublogo.png"/></a></div>',
 		'</div>',
