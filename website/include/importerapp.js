af814b11a55d351c9052e0ec6cc1d56a02ac0590
kovacsv <viktorkovacs@gmail.com>
2017-01-14 09:51:48 +0100

    Modify welcome text.

--- website/include/importerapp.js
+++ website/include/importerapp.js
@@ -90,7 +90,7 @@ ImporterApp.prototype.GetWelcomeText = function ()
 {
 	var welcomeText = [
 		'<div class="welcometitle">Welcome to Online 3D Viewer!</div>',
-		'<div class="welcometext">Here you can view your local 3D models online. Just simply drag and drop 3D files and textures to this browser window or use the open button above.</div>',
+		'<div class="welcometext">Here you can view your local 3D models online. You have three ways to open a file. Use the open button above to select files, simply drag and drop files to this browser window, or define the url of the files as location hash.</div>',
 		'<div class="welcometextformats">Supported formats: 3ds, obj, stl.</div>',
 		'<div class="welcometext">Powered by <a target="_blank" href="https://github.com/mrdoob/three.js/">Three.js</a> and <a target="_blank" href="https://github.com/kovacsv/JSModeler">JSModeler</a>.</div>',
 		'<div class="welcometext"><a target="_blank" href="https://github.com/kovacsv/Online3DViewer"><img src="images/githublogo.png"/></a></div>',
