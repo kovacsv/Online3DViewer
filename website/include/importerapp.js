53d0383621610a65dab9a8acc565e37df8f7d3af
kovacsv <viktorkovacs@gmail.com>
2019-05-11 21:55:08 +0200

    Update version.

--- website/include/importerapp.js
+++ website/include/importerapp.js
@@ -51,7 +51,7 @@ ImporterApp.prototype.Init = function ()
 	var myThis = this;
 	var top = document.getElementById ('top');
 	this.importerButtons = new ImporterButtons (top);
-	this.importerButtons.AddLogo ('Online 3D Viewer <span class="version">v 0.6.4</span>');
+	this.importerButtons.AddLogo ('Online 3D Viewer <span class="version">v 0.6.5</span>');
 	this.importerButtons.AddButton ('images/openfile.png', 'Open File', function () { myThis.OpenFile (); });
 	this.importerButtons.AddButton ('images/fitinwindow.png', 'Fit In Window', function () { myThis.FitInWindow (); });
 	this.importerButtons.AddToggleButton ('images/fixup.png', 'images/fixupgray.png', 'Enable/Disable Fixed Up Vector', function () { myThis.SetFixUp (); });
