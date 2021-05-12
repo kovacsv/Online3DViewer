6baf5b4767d676f812dc6e8e0eb7a81d1e39181b
Viktor Kovacs <viktorkovacs@gmail.com>
2021-05-12 20:09:52 +0200

    Run the correct python version with a node.js script.

---
+++ tools/run_python.js
@@ -0,0 +1,13 @@
+let childProcess = require ('child_process');
+
+let pythonExecutable = 'python';
+if (process.platform !== 'win32') {
+    pythonExecutable = 'python3';
+}
+
+let args = process.argv.slice (2);
+let result = childProcess.spawnSync (pythonExecutable, args, {
+    stdio: "inherit"
+});
+
+process.exit (result.status);
