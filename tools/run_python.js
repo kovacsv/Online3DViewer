fef0d8070cc505390e219d57a191933a97d3c30b
Viktor Kovacs <viktorkovacs@gmail.com>
2021-05-12 20:01:25 +0200

    Run build on travis.

--- tools/run_python.js
+++
@@ -1,8 +0,0 @@
-let cp = require ('child_process');
-let pythonExecutable = 'python';
-if (process.platform !== 'win32') {
-    pythonExecutable = 'python3';
-}
-cp.spawnSync (pythonExecutable, process.argv.slice (2), {
-    stdio: "inherit"
-});
