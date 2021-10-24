90539cc15d0bb7729156f284425f1ecdf5a978a3
kovacsv <viktorkovacs@gmail.com>
2021-10-24 18:08:50 +0200

    Move MeshInstanceId to a separate file.

---
+++ source/model/meshinstanceid.js
@@ -0,0 +1,13 @@
+OV.MeshInstanceId = class
+{
+    constructor (nodeId, meshIndex)
+    {
+        this.nodeId = nodeId;
+        this.meshIndex = meshIndex;
+    }
+
+    IsEqual (rhs)
+    {
+        return this.nodeId === rhs.nodeId && this.meshIndex === rhs.meshIndex;
+    }
+};
--- source/model/modelutils.js
+++ source/model/modelutils.js
@@ -1,17 +1,3 @@
-OV.MeshInstanceId = class
-{
-    constructor (nodeId, meshIndex)
-    {
-        this.nodeId = nodeId;
-        this.meshIndex = meshIndex;
-    }
-
-    IsEqual (rhs)
-    {
-        return this.nodeId === rhs.nodeId && this.meshIndex === rhs.meshIndex;
-    }
-};
-
 OV.CalculateTriangleNormal = function (v0, v1, v2)
 {
     let v = OV.SubCoord3D (v1, v0);
