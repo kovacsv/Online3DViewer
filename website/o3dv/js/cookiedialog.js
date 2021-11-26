494b05bb8e4f41091eb1a1398b3c5794c503e245
kovacsv <viktorkovacs@gmail.com>
2021-11-26 23:13:01 +0100

    Minor modification.

--- website/o3dv/js/cookiedialog.js
+++ website/o3dv/js/cookiedialog.js
@@ -12,7 +12,7 @@ OV.ShowCookieDialog = function (onAccept)
     ]);
 
     let text = 'This website uses cookies to offer you better user experience. See the details at the <a target="_blank" href="info/cookies.html">Cookies Policy</a> page.';
-    $('<div>').html (text).addClass ('ov_dialog_section').appendTo (contentDiv);
+    OV.AddDiv (contentDiv, 'ov_dialog_section', text);
     dialog.SetCloseable (false);
     dialog.Show ();
     return dialog;
