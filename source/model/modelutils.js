2d8acfe7da8c6eeef01e0b286ea64842316febe3
kovacsv <viktorkovacs@gmail.com>
2021-12-30 10:16:49 +0100

    Move mesh utils to a separate file.

--- source/model/modelutils.js
+++ source/model/modelutils.js
@@ -1,50 +1,3 @@
-OV.CalculateTriangleNormal = function (v0, v1, v2)
-{
-    let v = OV.SubCoord3D (v1, v0);
-    let w = OV.SubCoord3D (v2, v0);
-    let normal = OV.CrossVector3D (v, w);
-    normal.Normalize ();
-    return normal;
-};
-
-OV.TransformMesh = function (mesh, transformation)
-{
-    if (transformation.IsIdentity ()) {
-        return;
-    }
-
-    for (let i = 0; i < mesh.VertexCount (); i++) {
-        let vertex = mesh.GetVertex (i);
-        let transformed = transformation.TransformCoord3D (vertex);
-        vertex.x = transformed.x;
-        vertex.y = transformed.y;
-        vertex.z = transformed.z;
-    }
-
-    if (mesh.NormalCount () > 0) {
-        let trs = transformation.GetMatrix ().DecomposeTRS ();
-        let normalMatrix = new OV.Matrix ().ComposeTRS (new OV.Coord3D (0.0, 0.0, 0.0), trs.rotation, new OV.Coord3D (1.0, 1.0, 1.0));
-        let normalTransformation = new OV.Transformation (normalMatrix);
-        for (let i = 0; i < mesh.NormalCount (); i++) {
-            let normal = mesh.GetNormal (i);
-            let transformed = normalTransformation.TransformCoord3D (normal);
-            normal.x = transformed.x;
-            normal.y = transformed.y;
-            normal.z = transformed.z;
-        }
-    }
-};
-
-OV.FlipMeshTrianglesOrientation = function (mesh)
-{
-    for (let i = 0; i < mesh.TriangleCount (); i++) {
-        let triangle = mesh.GetTriangle (i);
-        let tmp = triangle.v1;
-        triangle.v1 = triangle.v2;
-        triangle.v2 = tmp;
-    }
-};
-
 OV.IsModelEmpty = function (model)
 {
     let isEmpty = true;
@@ -54,40 +7,6 @@ OV.IsModelEmpty = function (model)
         }
     });
     return isEmpty;
-};
-
-OV.CloneMesh = function (mesh)
-{
-    let cloned = new OV.Mesh ();
-
-    cloned.SetName (mesh.GetName ());
-
-    for (let i = 0; i < mesh.VertexCount (); i++) {
-        let vertex = mesh.GetVertex (i);
-        cloned.AddVertex (vertex.Clone ());
-    }
-
-    for (let i = 0; i < mesh.VertexColorCount (); i++) {
-        let color = mesh.GetVertexColor (i);
-        cloned.AddVertexColor (color.Clone ());
-    }
-
-    for (let i = 0; i < mesh.NormalCount (); i++) {
-        let normal = mesh.GetNormal (i);
-        cloned.AddNormal (normal.Clone ());
-    }
-
-    for (let i = 0; i < mesh.TextureUVCount (); i++) {
-        let uv = mesh.GetTextureUV (i);
-        cloned.AddTextureUV (uv.Clone ());
-    }
-
-    for (let i = 0; i < mesh.TriangleCount (); i++) {
-        let triangle = mesh.GetTriangle (i);
-        cloned.AddTriangle (triangle.Clone ());
-    }
-
-    return cloned;
 };
 
 OV.GetBoundingBox = function (object3D)
