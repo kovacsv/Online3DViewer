9aff0be5f06608db2b38e67c93230a715c400cc5
kovacsv <viktorkovacs@gmail.com>
2022-05-18 20:19:02 +0200

    Show assembly structure of step files #265

--- source/engine/import/importerstp.js
+++ source/engine/import/importerstp.js
@@ -1,5 +1,6 @@
 import { Direction } from '../geometry/geometry.js';
 import { GetExternalLibPath } from '../io/externallibs.js';
+import { Node } from '../model/node.js';
 import { ColorFromFloatComponents, ColorToHexString } from '../model/color.js';
 import { PhongMaterial } from '../model/material.js';
 import { ConvertThreeGeometryToMesh } from '../threejs/threeutils.js';
@@ -68,28 +69,48 @@ export class ImporterStp extends ImporterBase
             material.color = color;
             return this.model.AddMaterial (material);
         });
-        for (let occtMesh of stepContent.meshes) {
-            let materialIndex = null;
-            if (occtMesh.color) {
-                let color = ColorFromFloatComponents (occtMesh.color[0], occtMesh.color[1], occtMesh.color[2]);
-                materialIndex = colorToMaterial.GetMaterialIndex (color);
-            }
-            let mesh = ConvertThreeGeometryToMesh (occtMesh, materialIndex);
-            if (occtMesh.name) {
-                mesh.SetName (occtMesh.name);
-            }
-            if (occtMesh.face_colors) {
-                for (let faceColorGroup of occtMesh.face_colors) {
-                    let faceColor = ColorFromFloatComponents (faceColorGroup.color[0], faceColorGroup.color[1], faceColorGroup.color[2]);
-                    let faceMaterialIndex = colorToMaterial.GetMaterialIndex (faceColor);
-                    for (let i = faceColorGroup.first; i <= faceColorGroup.last; i++) {
-                        let triangle = mesh.GetTriangle (i);
-                        triangle.SetMaterial (faceMaterialIndex);
-                    }
-                }
-            }
-            this.model.AddMeshToRootNode (mesh);
-        }
+        let rootNode = this.model.GetRootNode ();
+        this.ImportNode (stepContent, stepContent.root, rootNode, colorToMaterial);
         onFinish ();
 	}
+
+    ImportNode (stepContent, occtNode, parentNode, colorToMaterial)
+    {
+        for (let nodeMeshIndex of occtNode.meshes) {
+            let occtMesh = stepContent.meshes[nodeMeshIndex]
+            let mesh = this.ImportMesh (occtMesh, colorToMaterial);
+            let meshIndex = this.model.AddMesh (mesh);
+            parentNode.AddMeshIndex (meshIndex);
+        }
+        for (let childOcctNode of occtNode.children) {
+            let childNode = new Node ();
+            childNode.SetName (childOcctNode.name);
+            parentNode.AddChildNode (childNode);
+            this.ImportNode (stepContent, childOcctNode, childNode, colorToMaterial);
+        }
+    }
+
+    ImportMesh (occtMesh, colorToMaterial)
+    {
+        let materialIndex = null;
+        if (occtMesh.color) {
+            let color = ColorFromFloatComponents (occtMesh.color[0], occtMesh.color[1], occtMesh.color[2]);
+            materialIndex = colorToMaterial.GetMaterialIndex (color);
+        }
+        let mesh = ConvertThreeGeometryToMesh (occtMesh, materialIndex);
+        if (occtMesh.name) {
+            mesh.SetName (occtMesh.name);
+        }
+        if (occtMesh.face_colors) {
+            for (let faceColorGroup of occtMesh.face_colors) {
+                let faceColor = ColorFromFloatComponents (faceColorGroup.color[0], faceColorGroup.color[1], faceColorGroup.color[2]);
+                let faceMaterialIndex = colorToMaterial.GetMaterialIndex (faceColor);
+                for (let i = faceColorGroup.first; i <= faceColorGroup.last; i++) {
+                    let triangle = mesh.GetTriangle (i);
+                    triangle.SetMaterial (faceMaterialIndex);
+                }
+            }
+        }
+        return mesh;
+    }
 }
