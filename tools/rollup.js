c2e41d1e0bf387158182253839c31d5d9b32b223
kovacsv <viktorkovacs@gmail.com>
2023-01-09 22:38:16 +0100

    Create one module file with rollup so the jsdoc comments will be preserved when using it as a module.

---
+++ tools/rollup.js
@@ -0,0 +1,19 @@
+export default [
+    {
+        input: 'source/engine/main.js',
+        output: {
+            file: 'build/o3dv.module.js',
+            format: 'es'
+        },
+        external: [
+            'fflate',
+            'three',
+            'three/examples/jsm/loaders/SVGLoader.js',
+            'three/examples/jsm/loaders/TGALoader.js',
+            'three/examples/jsm/loaders/FBXLoader.js',
+            'three/examples/jsm/loaders/ColladaLoader.js',
+            'three/examples/jsm/loaders/VRMLLoader.js',
+            'three/examples/jsm/loaders/3MFLoader.js'
+        ]
+    }
+];
