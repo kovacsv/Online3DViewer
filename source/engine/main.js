c95f7c202688ad2df14639f196e80f4db026bce2
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2023-01-15 03:09:07 +0600

    Allow initializing viewer element from File objects. (#352)
    Co-authored-by: Ilia Pozdnyakov <pozdnyakov@peterpartner.net>

--- source/engine/main.js
+++ source/engine/main.js
@@ -66,7 +66,7 @@ import { ThreeModelLoader } from './threejs/threemodelloader.js';
 import { HasHighpDriverIssue, GetShadingType, ConvertThreeColorToColor, ConvertColorToThreeColor, ConvertThreeGeometryToMesh, DisposeThreeObjects, ShadingType } from './threejs/threeutils.js';
 import { Camera, CameraIsEqual3D, CameraMode } from './viewer/camera.js';
 import { GetIntegerFromStyle, GetDomElementExternalWidth, GetDomElementExternalHeight, GetDomElementInnerDimensions, GetDomElementClientCoordinates, CreateDomElement, AddDomElement, AddDiv, ClearDomElement, InsertDomElementBefore, InsertDomElementAfter, ShowDomElement, IsDomElementVisible, SetDomElementWidth, SetDomElementHeight, GetDomElementOuterWidth, GetDomElementOuterHeight, SetDomElementOuterWidth, SetDomElementOuterHeight, CreateDiv } from './viewer/domutils.js';
-import { EmbeddedViewer, Init3DViewerElement, Init3DViewerElements } from './viewer/embeddedviewer.js';
+import { EmbeddedViewer, Init3DViewerElementFromUrlList, Init3DViewerElements } from './viewer/embeddedviewer.js';
 import { MouseInteraction, TouchInteraction, ClickDetector, Navigation, NavigationType } from './viewer/navigation.js';
 import { EnvironmentSettings, ShadingModel } from './viewer/shadingmodel.js';
 import { CameraValidator, UpVector, Viewer, GetDefaultCamera, TraverseThreeObject, GetShadingTypeOfObject } from './viewer/viewer.js';
@@ -312,7 +312,7 @@ export {
     SetDomElementOuterHeight,
     CreateDiv,
     EmbeddedViewer,
-    Init3DViewerElement,
+    Init3DViewerElementFromUrlList,
     Init3DViewerElements,
     MouseInteraction,
     TouchInteraction,
