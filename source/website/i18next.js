8f1f1cd823961e5d286a371906272aef368d1a3c
Axetroy <axetroy.dev@gmail.com>
2024-04-08 22:31:54 +0800

    fix: eslint

--- source/website/i18next.js
+++ source/website/i18next.js
@@ -1,19 +1,19 @@
-import i18next from "i18next";
-import { CookieGetStringVal } from "./cookiehandler.js";
+import i18next from 'i18next';
+import { CookieGetStringVal } from './cookiehandler.js';
 
-import zhCN from "./locale/zh-CN.json" assert { type: "json" };
+import zhCN from './locale/zh-CN.json' assert { type: 'json' };
 
 const defaultLang =
-  typeof navigator !== "undefined"
-    ? CookieGetStringVal("ov_language", navigator.language)
-    : "";
+  typeof navigator !== 'undefined'
+    ? CookieGetStringVal('ov_language', navigator.language)
+    : '';
 
 i18next.init({
   lng: defaultLang, // if you're u  sing a language detector, do not define the lng option
   debug: false,
-  supportedLngs: ["zh-CN"],
+  supportedLngs: ['zh-CN'],
   resources: {
-    "zh-CN": { translation: zhCN },
+    'zh-CN': { translation: zhCN },
   },
 });
 
@@ -21,11 +21,11 @@ const t = i18next.t;
 
 export { i18next, t };
 
-if (typeof document !== "undefined") {
+if (typeof document !== 'undefined') {
   // translate HTML
-  document.addEventListener("DOMContentLoaded", () => {
-    document.querySelectorAll("[data-i18n]").forEach((ele) => {
-      const translateKey = ele.getAttribute("data-i18n") || ele.textContent;
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('[data-i18n]').forEach((ele) => {
+      const translateKey = ele.getAttribute('data-i18n') || ele.textContent;
       ele.textContent = t(translateKey);
     });
   });
