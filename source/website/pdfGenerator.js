724fbe75b23ab3f98e25c0ee7bae80681e1887e8
Tomas1337 <tomas.lastrilla@gmail.com>
2024-07-30 14:38:58 +0800

    quote style change

--- source/website/pdfGenerator.js
+++ source/website/pdfGenerator.js
@@ -52,13 +52,13 @@ async function generatePdf(data, isForPatient = false) {
     const indent = 20;
 
     const introText = isForPatient 
-    ? "Hi ${patientName || ''},"
-    : "Hi ${sendToEmail || ''},";
+        ? `Hi ${patientName || ''},`
+        : `Hi ${sendToEmail || ''},`;
 
-    const introBody = isForPatient
-        ? "Thank you for using TellMeWhereItHurtsNow.com to monitor and manage your pain. Pain can significantly impact one's quality of life. Understanding its location and intensity helps in diagnosing and managing the underlying causes effectively."
-        : "Your acquaintance, ${patientName || ''}, has shared with you a snapshot describing their pain and where they are feeling it. Pain can significantly impact one's quality of life. Understanding its location and intensity helps in diagnosing and managing the underlying causes effectively.";
-    
+const introBody = isForPatient
+        ? `Thank you for using TellMeWhereItHurtsNow.com to monitor and manage your pain. Pain can significantly impact one's quality of life. Understanding its location and intensity helps in diagnosing and managing the underlying causes effectively.`
+        : `Your acquaintance, ${patientName || ''}, has shared with you a snapshot describing their pain and where they are feeling it. Pain can significantly impact one's quality of life. Understanding its location and intensity helps in diagnosing and managing the underlying causes effectively.`;
+
     const wrappedText = wrapText(introText, helveticaBoldFont, 12, introWidth);
     wrappedText.forEach((line, index) => {
         const y = introStartY - index * lineHeight;
